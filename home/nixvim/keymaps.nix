9570e95dce35a380a7ac8a42cbb0c1d5df3bebc6
Melker Isaksson <Melker.Isaksson@outlook.com>
2025-04-18 12:49:47 +0200

    LSP keymaps

--- home/nixvim/keymaps.nix
+++ home/nixvim/keymaps.nix
@@ -28,5 +28,61 @@
       action = "<cmd>Telescope live_grep<CR>";
       options.silent = true;
     }
+
+    # LSP
+    {
+      mode = "n";
+      key = "gd";
+      action = "vim.lsp.buf.definition";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "gD";
+      action = "vim.lsp.buf.declaration";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "gr";
+      action = "<cmd>Telescope lsp_references<CR>";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "K";
+      action = "vim.lsp.buf.hover";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>ca";
+      action = "vim.lsp.buf.code_action";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>rn";
+      action = "vim.lsp.buf.rename";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = "vim.diagnostic.open_float";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "]d";
+      action = "vim.diagnostic.goto_next";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "[d";
+      action = "vim.diagnostic.goto_prev";
+      options.silent = true;
+    }
   ];
 }
