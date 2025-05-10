c757a6cb2a7b8992b92b811cbacf350fda3ad7ab
Melker Isaksson <Melker.Isaksson@outlook.com>
2025-05-10 21:29:42 +0200

    Added and changed configuration of some plugins

--- home/nixvim/config.nix
+++ home/nixvim/config.nix
@@ -20,19 +20,27 @@
       oil.enable = true;
       lualine.enable = true;
       which-key.enable = true;
+      bufferline.enable = true;
+      lsp-lines.enable = true;
   
       wtf.enable = true;                # AI-driven dignostics debugging
 
       cmp = {
         enable = true;
         autoEnableSources = true;
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+          };
+        };
       };
-
-      cmp-nvim-lsp.enable = true;
-
-      cmp-buffer.enable = true;
-      cmp-path.enable = true;
-      cmp_luasnip.enable = true;
       luasnip.enable = true;
     };
 
